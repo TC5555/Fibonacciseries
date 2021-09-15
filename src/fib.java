89eb63680932fc2ac75480a5f36aa99305e62014
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-15 10:43:34 -0700

    .

--- src/fib.java
+++ src/fib.java
@@ -1,12 +1,13 @@
 public class fib {
     public static void main(String[] args) {
-        int i = 1;
-        int e = 1;
-        for (int j  = 1; j <= 6; j++) {
-            System.out.print(e + " ");
-            e = i + e;
-            System.out.print(i + " ");
-            i = i + e;
+        for (int k = 0; k < 6; k++) {
+            int i = 1;
+            int e = 1;
+            for (int j  = 1; j <= k; j++) {
+                i = i + e;
+                e = i + e;
+            }
+            System.out.print(i + " " + e + " ");
         }
     }
 }
